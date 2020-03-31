9c886113fc32d507962307aea5da06c95388722d
Your Name <artger@ukr.net>
2020-03-31 11:33:09 +0300

    spider visit all page

--- scrapy_versace/scrapy_versace/spiders/quotes_spider.py
+++ scrapy_versace/scrapy_versace/spiders/quotes_spider.py
@@ -3,14 +3,9 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-
-    def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+    ]
 
     def parse(self, response):
         for quote in response.css("div.quote"):
@@ -19,3 +14,7 @@ class QuotesSpider(scrapy.Spider):
                 'author': quote.css('small.author::text').get(),
                 'tags': quote.css('div.tags a.tag::text').getall(),
             }
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(url=next_page, callback=self.parse)
