7d00ec5cc6e4b0782a4c9e44e1a4fece55d79460
Your Name <artger@ukr.net>
2020-03-31 17:48:58 +0300

    question commit

--- scrapy_versace/scrapy_versace/spiders/quotes_spider.py
+++ scrapy_versace/scrapy_versace/spiders/quotes_spider.py
@@ -3,18 +3,19 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-    start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-    ]
+    allowed_domains = ["https://www.versace.com"]
+    start_urls = ['https://www.versace.com/us/en-us/home/',]
 
     def parse(self, response):
-        for quote in response.css("div.quote"):
+        page_link = response.css("li.level-3-item a::attr(href)").getall()
+        for i in page_link:
+            next_page = response.urljoin(i)
+            print(next_page)
+            yield scrapy.Request(url=next_page, callback=self.parse_item)
+
+    def parse_item(self, response):
+        for item in response.css("div.product-name"):
             yield {
-                'text': quote.css('span.text::text').get(),
-                'author': quote.css('small.author::text').get(),
-                'tags': quote.css('div.tags a.tag::text').getall(),
+                'name': item.css("a.name-link::text").get(),
+                'href': item.css("a.name-link::attr(href)").get(),
             }
-        next_page = response.css("li.next a::attr(href)").get()
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(url=next_page, callback=self.parse)
