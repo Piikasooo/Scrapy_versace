b49d6597f588a5f1c3e3d0109a7b2e11b350b6f0
Your Name <artger@ukr.net>
2020-04-07 11:57:24 +0300

    scrapy items

--- scrapy_versace/scrapy_versace/spiders/quotes_spider.py
+++ scrapy_versace/scrapy_versace/spiders/quotes_spider.py
@@ -13,10 +13,20 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ['https://www.versace.com/us/en-us/home/', ]
 
     def parse(self, response):
-        link = ItemLoader(item=Href(), response=response)
-        link.add_css('href', "li.view-all-item a::attr(href)")
-        link.add_css('href', "li.children-category a.subcategory-image-link::attr(href)")
-        return link.load_item()
+        for next_page in response.css("li.view-all-item a::attr(href)").getall():
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse_link_on_item)
+
+        for next_page in response.css("li.children-category a.subcategory-image-link::attr(href)").getall():
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse_link_on_item)
+
+    def parse_link_on_item(self, response):
+        for next_page in response.css("ul.search-result-items li.js-grid-tile "
+                                      "div.product-information-wrapper div.product-name a::attr(href)").getall():
+            next_page = response.urljoin(next_page)
+            print(next_page)
+            yield scrapy.Request(next_page, callback=self.parse_item)
 
     def parse_item(self, response):
         product = ItemLoader(item=Product(), response=response)
