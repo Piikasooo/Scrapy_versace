9ea539a7c1f74bf50b9a96f1412a2d7d1fb819a4
Your Name <artger@ukr.net>
2020-04-07 10:02:41 +0300

    save in csv

--- scrapy_versace/scrapy_versace/pipelines.py
+++ scrapy_versace/scrapy_versace/pipelines.py
@@ -4,17 +4,8 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-import json
 
 
 class ScrapyVersacePipeline(object):
-    def open_spider(self, spider):
-        self.file = open('item.jl', "w")
-
-    def close_spider(self, spider):
-        self.file.close()
-
     def process_item(self, item, spider):
-        line = json.dumps(dict(item)) + "\n"
-        self.file.write(line)
         return item
--- scrapy_versace/scrapy_versace/spiders/quotes_spider.py
+++ scrapy_versace/scrapy_versace/spiders/quotes_spider.py
@@ -5,6 +5,10 @@ from scrapy.loader import ItemLoader
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
+    custom_settings = {
+        'FEED_FORMAT': 'csv',
+        'FEED_URI': 'feed-test.csv'
+    }
     allowed_domains = ["https://www.versace.com/"]
     start_urls = ['https://www.versace.com/us/en-us/jeans-couture/new-arrivals/new-in-for-her/logo-belt-bag-e899/EE1VVBBL3-E71411_ENU_NR_E899__.html?cgid=11000041#start=1', ]
 
